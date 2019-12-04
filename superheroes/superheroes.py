d811f91ba00e253946d61d15393f20bc8449d0eb
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2019-12-03 19:04:58 -0800

    adding attack

--- superheroes/superheroes.py
+++ superheroes/superheroes.py
@@ -33,11 +33,19 @@ class Hero:
     def add_weapon(self, weapon):
         self.abilities.append(weapon)
 
+    def current_health(self):
+        return self.current_health
+
+    def attack(self):
+        total_attack = 0
+        for ability in self.abilities:
+            total_attack += ability.attack()
+        return total_attack
+
     def add_armor(self, armor):
-        self.abilities.append(ability)
+        self.abilities.append(armor)
 
-
-
+    def defend
 
 
 
@@ -64,19 +72,26 @@ class Hero:
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    ability = Ability(“Debugging Ability”, 20)
-    print(ability.name)
-    print(ability.attack())
+    # ability = Ability("Debugging Ability", 20)
+    # print(ability.name)
+    # print(ability.attack())
+    #
+    # armor = Armor("Debugging Shield", 10)
+    # print(armor.name)
+    # print(armor.block())
+    #
+    # my_hero = Hero("Grace Hopper", 200)
+    # print(my_hero.name)
+    # print(my_hero.current_health)
+    #
+    # ability = Ability("Great Debugging", 50)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # print(hero.abilities)
 
-    armor = Armor(“Debugging Shield”, 10)
-    print(armor.name)
-    print(armor.block())
-
-    my_hero = Hero("Grace Hopper", 200)
-    print(my_hero.name)
-    print(my_hero.current_health)
-
-    ability = Ability("Great Debugging", 50)
-    hero = Hero("Grace Hopper", 200)
-    hero.add_ability(ability)
-    print(hero.abilities)
+    # ability = Ability("Great Debugging", 50)
+    # another_ability = Ability("Smarty Pants", 90)
+    # hero = Hero("Grace Hopper", 200)
+    # hero.add_ability(ability)
+    # hero.add_ability(another_ability)
+    # print(hero.attack())
